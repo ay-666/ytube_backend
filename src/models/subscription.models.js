cc240d4e3d896fa439cb592b3fbe0c763b2c697d
Arpit Yadav <113028039+ay-666@users.noreply.github.com>
2025-02-18 09:26:07 +0000

    added subscription model

---
+++ src/models/subscription.models.js
@@ -0,0 +1,17 @@
+import mongoose , {Schema} from "mongoose";
+
+const subscriptionSchema = new Schema({
+  subscriber:{
+    type: Schema.Types.ObjectId, // user who is subscribing
+    ref: 'User'
+  },
+  channel:{
+    type:Schema.Types.ObjectId, // user with channel where people are subscribing
+    ref:'User'
+  }
+},{timestamps:true});
+
+
+export const Subscription = Schema.model('Subscription',subscriptionSchema);
+
+
